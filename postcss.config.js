acd91c5d91d528dae2e6c2dc1199c959566697e6
quentinhocde <quentinhcd@gmail.com>
2025-11-26 15:21:04 +0100

    Remove postcss import glob plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 import postcss from 'postcss';
 import postcssComment from 'postcss-comment';
-import postcssImportExtGlob from 'postcss-import-ext-glob';
 import postcssImport from 'postcss-import';
 import autoprefixer from 'autoprefixer';
 
@@ -10,10 +9,7 @@ export default ({ env }) => ({
     {
       postcssPlugin: 'grouped',
       Once(root, { result }) {
-        return postcss([postcssImportExtGlob, postcssImport]).process(
-          root,
-          result.opts,
-        );
+        return postcss([postcssImport]).process(root, result.opts);
       },
     },
     autoprefixer,
