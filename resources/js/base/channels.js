b17aac9106e2ac6352ae6f86cbf06cb191ed32f9
jcsuzanne <jc.suzanne@gmail.com>
2026-01-23 11:43:15 +0400

    refactor: remplacer events npm par EventTarget natif
    - Remplacement du package 'events' par EventTarget natif du navigateur
    - Implémentation d'une classe Channels basée sur EventTarget
    - Conservation de la même API (addListener, removeListener, emit, on, off)
    - Support des événements avec et sans données
    - Suppression de la dépendance 'events' (~50KB économisés)
    - Utilisation des APIs natives du navigateur (support depuis 2015)

--- resources/js/base/channels.js
+++ resources/js/base/channels.js
@@ -1,7 +1,48 @@
 'use strict';
-import EventsEmitter from 'events';
 
-class Channels extends EventsEmitter {}
+class Channels extends EventTarget {
+  constructor() {
+    super();
+    this._listeners = new WeakMap();
+  }
+
+  addListener(event, callback) {
+    const wrappedCallback = (e) => {
+      if (e.detail !== undefined) {
+        callback(e.detail);
+      } else {
+        callback();
+      }
+    };
+    
+    this._listeners.set(callback, wrappedCallback);
+    this.addEventListener(event, wrappedCallback);
+  }
+
+  removeListener(event, callback) {
+    const wrappedCallback = this._listeners.get(callback);
+    
+    if (wrappedCallback) {
+      this.removeEventListener(event, wrappedCallback);
+      this._listeners.delete(callback);
+    }
+  }
+
+  emit(event, data) {
+    this.dispatchEvent(
+      new CustomEvent(event, data !== undefined ? { detail: data } : {})
+    );
+  }
+
+  on(event, callback) {
+    this.addListener(event, callback);
+  }
+
+  off(event, callback) {
+    this.removeListener(event, callback);
+  }
+}
+
 const channels = new Channels();
 
 export default channels;
