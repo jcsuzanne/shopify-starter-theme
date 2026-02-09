7c3890d0b09ba48da5581e7393a67c21236b2ba5
jcsuzanne <jc.suzanne@gmail.com>
2026-02-09 14:32:07 +0400

    Update avant SK et LART (suite au dev Landa)

--- resources/js/styles.js
+++
@@ -1,6 +0,0 @@
-// Import a folder
-import.meta.glob(`../css/settings/*.css`, {eager: true});
-import.meta.glob(`../css/vendors/*.css`, {eager: true});
-import.meta.glob(`../css/base/*.css`, {eager: true});
-import.meta.glob(`../css/common/*.css`, {eager: true});
-import.meta.glob(`../css/utilities/*.css`, {eager: true});
